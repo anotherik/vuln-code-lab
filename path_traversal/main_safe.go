f984fa370ff7501036fa4b827dca68728dfa7ee5
Ricardo <ricardojotagoncalves@hotmail.com>
2025-05-25 04:04:14 +0100

    Add files via upload

---
+++ path_traversal/main_safe.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"path/filepath"
+	"strings"
+)
+
+const baseDir = "./files"
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	filename := r.URL.Query().Get("file")
+
+	if filename == "" {
+		http.Error(w, "Missing file parameter", http.StatusBadRequest)
+		return
+	}
+
+	cleanPath := filepath.Clean(filepath.Join(baseDir, filename))
+	if !strings.HasPrefix(cleanPath, filepath.Clean(baseDir)) {
+		http.Error(w, "Access denied", http.StatusForbidden)
+		return
+	}
+
+
+	data, err := ioutil.ReadFile(cleanPath)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error reading file: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write(data)
+}
+
+func main() {
+	http.HandleFunc("/view", viewHandler)
+
+	fmt.Println("Server running at http://localhost:8090/view?file=yourfile.txt")
+	log.Fatal(http.ListenAndServe(":8090", nil))
+}
